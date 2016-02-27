c45bf0d72267b47ac9e6def395081149294d52b8
Alan <nhnam.develop@gmail.com>
2016-02-27 18:03:16 +0800

    Finished

---
+++ RmdRevealViewController/RmdMenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RmdMenuViewController.swift
+//  RmdRevealViewController
+//
+//  Created by Nguyen Hoang Nam on 27/2/16.
+//  Copyright © 2016 Alan Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ RmdRevealViewController/RmdViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RmdViewController.swift
+//  RmdRevealViewController
+//
+//  Created by Nguyen Hoang Nam on 27/2/16.
+//  Copyright © 2016 Alan Nguyen. All rights reserved.
+//
+
+import Foundation
